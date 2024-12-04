088045aadba707f31455183e6d26a18ec8774684
Wai Yi <wongwaverley@gmail.com>
2024-12-04 16:14:34 +0000

    keep both keys

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECRET_KEY = 'django-insecure-7+ky++&cl7l&&vt1mh2%rlpl5gc=9k58zr1sjhr!of&@@8vw$n'
+SECRET_KEY = 'django-insecure-7+ky++&cl7l&&vt1mh2%rlpl5gc=9k58zr1sjhr!of&@@8vw$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

15d3f1bc44c6c58cf27eba59e2f68df15ea03051
Wai Yi <wongwaverley@gmail.com>
2024-12-05 10:29:58 +0000

    add f strings to post and comment title and author

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
         ordering = ["created_on"]
     
     def __str__(self):
-        return f"The title of this post is {self.title}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -33,3 +33,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"

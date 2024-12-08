6b5b1e0b8f34cb09b73b8d6f0ccb391a6001036f
Wai Yi <wongwaverley@gmail.com>
2024-12-08 00:04:57 +0000

    add template, base and index.html

--- blog/views.py
+++ blog/views.py
@@ -8,4 +8,5 @@ from .models import Post
 #     return HttpResponse("Hello, Blog!")
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6

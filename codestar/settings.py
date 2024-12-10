110782cc1ce43f75ec830e5b9f18c9186d46e3e4
Wai Yi <wongwaverley@gmail.com>
2024-12-10 09:22:36 +0000

    add - to python runtime.txt

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECRET_KEY = 'django-insecure-7+ky++&cl7l&&vt1mh2%rlpl5gc=9k58zr1sjhr!of&@@8vw$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

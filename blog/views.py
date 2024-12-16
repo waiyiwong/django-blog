39d005ee413deeca4818f8e93d1cb9c23beaa903
Wai Yi <wongwaverley@gmail.com>
2024-12-16 23:44:04 +0000

    Handle the POST request from the comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 # from django.http import HttpResponse
 
 # # Create your views here.
@@ -30,6 +31,15 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+    
+    comment_form = CommentForm()
 
 
     return render(
@@ -37,5 +47,7 @@ def post_detail(request, slug):
         "blog/post_detail.html",
         {"post": post,
         "comments": comments,
-        "comment_count": comment_count},
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+        },
     )
